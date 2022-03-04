e14d74908d6a9e24ff15c1b8cb8b97b16fc3fb8e
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2022-03-04 20:23:42 -0300

    (04/03)Sequencia Binária

--- 01_busca_sequencial.py
+++ 01_busca_sequencial.py
@@ -1,3 +1,8 @@
+from data.primos import primos
+from data.lista_nomes import nomes
+from time import time
+
+
 # BUSCA SEQUENCIAL
 #
 # Procura por um valor na lista, partindo do primeiro elemento até o último. Retorna:
@@ -21,4 +26,36 @@ def busca_sequencial(val, lista):
 
 print(f'Posição de 27: {busca_sequencial(27, nums)}')
 print(f'Posição de 27: {busca_sequencial(40, nums)}')
+
+
+# Buscando números primos
+# hora_ini = time()
+# print(f"Posição de 8008: {busca_sequencial(8008, primos)}")
+# hora_fim = time()
+# print(f"Tempo gasto para buscar 8008: {(hora_fim - hora_ini) * 1000}ms")
+
+# hora_ini = time()
+# print(f"Posição de 1487: {busca_sequencial(1487, primos)}")
+# hora_fim = time()
+# print(f"Tempo gasto para buscar 1487: {(hora_fim - hora_ini) * 1000}ms")
+
+# hora_ini = time()
+# print(f"Posição de 7603: {busca_sequencial(7603, primos)}")
+# hora_fim = time()
+# print(f"Tempo gasto para buscar 7603: {(hora_fim - hora_ini) * 1000}ms")
+
+hora_ini = time()
+print(f"Posição de ORKUTILSON: {busca_sequencial('ORKUTILSON', nomes)}")
+hora_fim = time()
+print(f"Tempo gasto para buscar ORKUTILSON: {(hora_fim - hora_ini) * 1000}ms")
+
+hora_ini = time()
+print(f"Posição de FAUSTO: {busca_sequencial('FAUSTO', nomes)}")
+hora_fim = time()
+print(f"Tempo gasto para buscar FAUSTO: {(hora_fim - hora_ini) * 1000}ms")
+
+hora_ini = time()
+print(f"Posição de ZULEICA: {busca_sequencial('ZULEICA', nomes)}")
+hora_fim = time()
+print(f"Tempo gasto para buscar ZULEICA: {(hora_fim - hora_ini) * 1000}ms")
 

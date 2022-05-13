d3eab333662870b55cada6d2d60ee6a7edf4f836
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2022-05-13 19:53:25 -0300

    doubly_linked_list nodo

--- lib/doubly_linked_list.py
+++ lib/doubly_linked_list.py
@@ -2,12 +2,15 @@
     Classe que representa uma unidade de informação na lista duplamente encadeada.
 '''
 
+from numpy import insert
+
+
 class Node:
 
     ''' Método construtor '''
-    def __init__(self):
+    def __init__(self, data):
         self.prev = None    # ponteiro para nodo anterior
-        self.data = None    # Dado do usuário
+        self.data = data    # Dado do usuário
         self.next = None    # ponteiro para nodo seguinte
 
 ######################################################################
@@ -29,3 +32,54 @@ class DoublyLinkedList:
         self.__head = None # Aponta para o primeiro nodo da lista
         self.__tail = None # Aponta para o último nodo da lista
         self.__count = 0    # Mantém o número de nodos da lista
+
+    '''
+        Propriedade que retorna a quantidade de itens da lista
+    '''
+    @property
+    def count(self):
+        return self.__count
+    
+    '''
+        Propriedade que retorna se a lista está ou não vazia 
+    '''
+    @property
+    def is_empty(self):
+        return self.__count == 0
+
+    '''
+        Método que insere um valor na posição especificada
+    '''
+    def insert(self, pos, val):
+        # Criamos um nodo que contém a informação do usuário e também
+        # os ponteiros prev e next apontando para o None
+        inserted = Node(val)
+
+        # 1° Caso: Lista vazia e este é o primeiro nodo a ser inserido
+        if self.is_empty:
+            self.__head = inserted
+            self.__tail = inserted
+        
+        # 2° Caso: Inserção na posição inicial (posição)
+        elif pos == 0:
+            inserted.next = self.__head
+            self.__head.prev = inserted
+            self.__head = inserted
+
+        # 3° Caso: Inserção na posição final (qualquer posição >= count)
+        elif pos >= self.count:
+            inserted.prev = self.__tail
+            self.__tail.next = inserted
+            self.__tail = inserted
+
+        # 4° Caso: Inserção na posição intermediária (entre os nodos)
+        elif:
+            # Encontrar o nodo anterior à posição de inserção
+
+        
+        # Incrementa a contagem de itens
+        self.__count += 1
+    
+    '''
+       Função privada que encontra o nodo da posição especificada 
+    '''
